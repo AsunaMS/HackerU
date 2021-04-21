4435c2d698b72a6b1c2c3be4b382e4f7844136d3
AsunaMS <73134868+AsunaMS@users.noreply.github.com>
2021-04-21 16:59:29 -0700

    Update LoginActivity.java

--- חומר לתרגולים - אנדרואיד/LoginActivity & Layout/LoginActivity.java
+++ חומר לתרגולים - אנדרואיד/LoginActivity & Layout/LoginActivity.java
@@ -125,28 +125,5 @@ public class LoginActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static String encryptPassword(String password) {
-        String sha1 = "";
-        try {
-            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
-            crypt.reset();
-            crypt.update(password.getBytes("UTF-8"));
-            sha1 = byteToHex(crypt.digest());
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return sha1;
-    }
-
-    public static String byteToHex(final byte[] hash) {
-        Formatter formatter = new Formatter();
-        for (byte b : hash) {
-            formatter.format("%02x", b);
-        }
-        String result = formatter.toString();
-        formatter.close();
-        return result;
-    }
+  
 }
