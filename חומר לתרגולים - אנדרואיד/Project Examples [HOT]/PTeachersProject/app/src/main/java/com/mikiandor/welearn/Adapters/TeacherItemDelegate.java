62edf075f72e172d454df14a5de44e7ad8976bd6
Nadav <33123586+zaguri8@users.noreply.github.com>
2022-08-29 16:23:39 +0300

    clearance

--- חומר לתרגולים - אנדרואיד/Project Examples [HOT]/PTeachersProject/app/src/main/java/com/mikiandor/welearn/Adapters/TeacherItemDelegate.java
+++
@@ -1,5 +0,0 @@
-package com.mikiandor.welearn.Adapters;
-
-public interface TeacherItemDelegate  {
-    void refreshTeacherList();
-}
--- חומר לתרגולים - אנדרואיד/Project Examples [HOT]/PTeachersProject/app/src/main/java/com/mikiandor/welearn/finals/Finals.java
+++
@@ -1,7 +0,0 @@
-package com.mikiandor.welearn.finals;
-
-public class Finals {
-    public static final String[] SUBJECTS = {"","android","java","c","Full Stack","c#","PHP","React","Python"};
-    public static final String[] LEVELS = {"","Beginner","Advanced","Expert"};
-
-}
